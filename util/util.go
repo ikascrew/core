81c10007e5b7e84c3f1b09750f67b53a101aca11
secondarykey <secondarykey@gmail.com>
2020-12-16 08:52:15 +0900

    fix: utilへの機能追加

--- util/util.go
+++ util/util.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+	"bufio"
 	"image"
+	"os"
+	"unsafe"
 
 	"gocv.io/x/gocv"
 )
@@ -16,4 +19,19 @@ func ResizeImage(m gocv.Mat, w, h int) (gocv.Mat, error) {
 func WriteImage(f string, m gocv.Mat) error {
 	gocv.IMWrite(f, m)
 	return nil
+}
+
+func bstring(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func sbytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
+}
+
+func Input() string {
+	std := bufio.NewScanner(os.Stdin)
+	std.Scan()
+	text := std.Text()
+	return text
 }
